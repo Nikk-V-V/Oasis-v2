058fed0ea8d07c1f02cad615b260f76a1a3dcafc
nikk <nikola716@gmail.com>
2021-12-11 16:11:25 +0200

    update

--- src/app/layouts/oasis/pages/menu/menu.page.ts
+++ src/app/layouts/oasis/pages/menu/menu.page.ts
@@ -1,13 +1,47 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {style, trigger, state, transition, animate} from '@angular/animations';
 
 @Component({
   templateUrl: 'menu.page.html',
-  styleUrls: ['menu.page.scss']
+  styleUrls: ['menu.page.scss'],
+  animations: [
+    trigger('light', [
+      state('start', style({
+        boxShadow: 'inset 0 0 110px 400px black'
+      })),
+      state('end', style({
+        boxShadow: 'inset 0 0 110px 0 black'
+      })),
+      transition('start <=> end', animate(800))
+    ]),
+    trigger('top-buttons', [
+      state('start', style({
+        top: '-100%',
+        right: '-100%'
+      })),
+      state('end', style({
+        top: '10%',
+        right: '10%'
+      })),
+      transition('start <=> end', animate(800))
+    ]),
+    trigger('bottom-buttons', [
+      state('start', style({
+        bottom: '-100%',
+        left: '-100%'
+      })),
+      state('end', style({
+        bottom: '10%',
+        left: '10%'
+      })),
+      transition('start <=> end', animate(800))
+    ]),
+  ]
 })
-export class MenuPage implements  OnInit, OnDestroy{
+export class MenuPage implements  OnInit {
 
-  load = false;
+  state = 'start';
   path: string;
 
   constructor(
@@ -20,11 +54,7 @@ export class MenuPage implements  OnInit, OnDestroy{
       this.path = data.path;
     });
     setTimeout(() => {
-      this.load = true;
+      this.state = 'end';
     }, 500);
-  }
-
-  ngOnDestroy() {
-    this.load = false;
   }
 }
